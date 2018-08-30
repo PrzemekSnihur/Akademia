13f019bc7cf0b2d4c2c253b03f9d07b1ababe3ce
Snihur Przemyslaw <przemyslaw.snihur@tieto.com>
2018-08-30 15:26:43 +0200

    Integrate two versions of program

--- Python/Better.py
+++ Python/Better.py
@@ -3,6 +3,9 @@ import operator
 import ast
 import datetime
 import re
+import os
+import urllib
+import requests
 
 API_KEY = "ed573f66"
 
@@ -14,6 +17,7 @@ rating = ''
 titles = []#
 MovieDickt = {}
 SortedMovies = []
+forbiddenCharacters = [':', '?', '<', '>', '|', '"', '\\', '/', '*']
 
 while title != 'end':
     if re.findall(r',', title):
@@ -69,6 +73,29 @@ while title != 'end':
         if not results:
             print("Wrong movie")
             continue
+        title = results['title']
+        for character in title:
+            if character in forbiddenCharacters:
+                title = title.replace(character, "")
+        if not len(title):
+            print("Write name of your folder: ")
+            title = input()
+        newpath = f"C:/Movies/{title}"
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+        fout = open(f"{newpath}/{title}.txt", 'w')
+        fout.write("Title: " + results['title'] + "\n")
+        fout.write("Rating: " + results['imdb_rating'] + "\n")
+        fout.write("Runtime: " + results['runtime'] + "\n")
+        if results['released'] == "N/A":
+            fout.write("Released: N/A" + "\n")
+        else:
+            fout.write(
+                f"Released:  {datetime.datetime.strptime(results['released'],'%d %b %Y').strftime('%d.%m.%Y')}" + "\n")
+        fout.write("Popularity: " + results['imdb_votes'] + "\n")
+        poster = results['poster']
+        if not poster == "N/A":
+            urllib.request.urlretrieve(poster, f"{newpath}/{title}.jpg")
         print("Title: " + results['title'])
         print("Rating: " + results['imdb_rating'])
         print("Runtime: " + results['runtime'])
@@ -82,6 +109,6 @@ while title != 'end':
 
     print("Write movie titles: ")
     title = input()
-
+print("Have a nice day!")
 
     #Deadpool, Emoji, Matrix
